f0e0e8931b365a6a18e871819f8afb424739c44a
Blip Help - Blip <104581060+HelpCenter-Take@users.noreply.github.com>
2024-01-02 13:19:40 -0300

    Delete src/hooks directory

--- src/hooks/useCustomEvents/types.ts
+++
@@ -1,8 +0,0 @@
-export interface CustomEventParams<T> {
-    readonly eventName: string;
-    eventToListen?: (e: CustomEvent<T>) => void;
-    readonly target?: Window | Document | HTMLElement;
-    readonly options?: AddEventListenerOptions;
-}
-
-export type Target = Window | Document | HTMLElement;
